a43c3cb3910bc6279153174af79ae5b2e498d1ee
Ridwan Ajibola <rajibola1997@gmail.com>
2020-12-16 07:31:01 +0100

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,70 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import Bounce from 'react-reveal/Bounce';
+import { Link } from 'react-scroll';
+import PortfolioContext from '../../context/context';
+import reactLogo from '../../images/reactjs-icon.svg';
+
+const Header = () => {
+  const { hero } = useContext(PortfolioContext);
+  const { title, name, subtitle, cta } = hero;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="hero" className="jumbotron">
+      <Container>
+        <div className="background_circle" />
+
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <h1 className="hero-title">
+            {title || 'Front-end Mobile App\n & Web Developer.'}{' '}
+            {/* <span className="text-color-main">{name || 'Your Name'}</span> */}
+            <br />
+            <div className="subtitle">
+              {subtitle ||
+                "Hi, I'm Ridwan Ajibola. A react and react-native developer focused on building responsive cross-platform mobile applications that look the same across various devices and have buttery-smooth user experience."}
+            </div>
+          </h1>
+        </Fade>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+          <Link
+            to="about"
+            smooth
+            duration={1000}
+            style={{ display: `inline`, backgroundColor: 'green' }}
+          >
+            <div className="hero-cta button">
+              <div className="circle-button" />
+              <span className="cta-text">{cta || 'see my works'}</span>
+            </div>
+          </Link>
+        </Fade>
+      </Container>
+      <h1 className="logo-container">
+        <Fade right={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <img src={reactLogo} className="react-logo" />
+        </Fade>
+        <Fade top={isDesktop} bottom={isMobile} duration={1500} delay={400} distance="30px">
+          <Bounce top={isDesktop} bottom={isMobile} duration={2000} delay={700} distance="30px">
+            <div className="my-name">RA</div>
+          </Bounce>
+        </Fade>
+      </h1>
+    </section>
+  );
+};
+
+export default Header;
